323c4f8cc268b81631b4b28d7fe20d6c9afbed13
Arlindo Neto <arlindosilvaneto@gmail.com>
2019-03-12 16:59:51 -0300

    TTT-0007 [Arlindo] :: Implementing move rewind on game container

--- src/containers/game/game.test.js
+++ src/containers/game/game.test.js
@@ -3,33 +3,50 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, {shallow} from 'enzyme';
 import Game from './index';
-import Board from '../../components/board';
 import Panel from '../../components/panel';
-import Square from '../../components/square';
 
 describe('Game container', () => {
   Enzyme.configure({ adapter: new Adapter() });
 
-  const game, 
-    panel,
-    board, 
-    squares;
-
   beforeEach(() => {
-    game = shallow(<Game />);
-
-    panel = game.find(Panel);
-    board = game.find(Board);
-    squares = board.find(Square);
+    
   });
 
   it('should start a game for O player', () => {
-    expect(panel.find('.sprite-o-state').length).toEqual(1);
+    const game = shallow(<Game />);
+
+    expect(game.find(Panel).props().player).toBe('O');
   });
 
   it('should change the player after a square click', () => {
-    squares.simulate('click');
-    
-    expect(panel.find('.sprite-x-state').length).toEqual(1);
+    const game = shallow(<Game />);
+
+    game.instance().onSquareClick(0);
+
+    expect(game.find(Panel).props().player).toBe('X');
+  });
+
+  it('should reset the player after a reset click', () => {
+    const game = shallow(<Game />);
+
+    game.instance().onSquareClick(0);
+
+    expect(game.find(Panel).props().player).toBe('X');
+
+    game.instance().onGameReset();
+
+    expect(game.find(Panel).props().player).toBe('O');
+  });
+
+  it('should reset the player after a rewind click', () => {
+    const game = shallow(<Game />);
+
+    game.instance().onSquareClick(0);
+
+    expect(game.find(Panel).props().player).toBe('X');
+
+    game.instance().onRewindMove();
+
+    expect(game.find(Panel).props().player).toBe('O');
   });
 });
--- src/containers/game/index.jsx
+++ src/containers/game/index.jsx
@@ -1,1 +1,61 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, createContext } from 'react';
+
+import Board from '../../components/board';
+import Panel from '../../components/panel';
+
+export const GameContext = createContext();
+const defaultState = {
+    gameState: '---------',
+    player: 'O',
+    winner: '',
+    lastMoves: []
+}
+
+export default class Game extends Component {
+    state = defaultState;
+
+    onSquareClick = (index) => {
+        const player = this.state.player,
+            lastMoves = this.state.lastMoves;
+
+        const gameState = this.state.gameState.split('');
+        gameState[index] = player;
+
+        lastMoves.push(index);
+
+        this.setState({
+            gameState: gameState.join(''),
+            player: player === 'O' ? 'X' : 'O',
+            lastMoves
+        });
+    }
+
+    onGameReset = () => {
+        this.setState(defaultState);
+    }
+
+    onRewindMove = () => {
+        const lastMoves = this.state.lastMoves,
+            gameState = this.state.gameState.split(''),
+            player = this.state.player;
+
+        if(lastMoves.length > 0) {
+            gameState[lastMoves.pop()] = '-';
+
+            this.setState({
+                gameState: gameState.join(''),
+                player: player === 'O' ? 'X' : 'O',
+                lastMoves
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="game-wrapper">
+                <Board state={this.state.gameState} onClick={this.onSquareClick} />
+                <Panel player={this.state.player} onReset={this.onGameReset} onRewind={this.onRewindMove} />
+            </div>
+        );
+    }
+}
