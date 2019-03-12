e2bf9b58f4acdb14163047a9e1b434e55e16f9b6
Arlindo Neto <arlindosilvaneto@gmail.com>
2019-03-12 13:15:11 -0300

    TTT-0002 [Arlindo] :: Implementing board dummy component - TEST PASS

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -1,3 +1,23 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
+
+import Square from '../square/square';
 
 import './board.css';
+
+const emptyState = '---------',
+    emptyFunction = () => {};
+
+const Board = ({state, onClick = emptyFunction}) => {
+    state = state && state.length === 9 ? state : emptyState;
+    const squareStates = state.split('');
+
+    return (
+        <div className="board-wrapper">
+            {squareStates.map((squareState, index) => {
+                return <Square key={`square_${index}`} state={squareState} onClick={() => onClick(index)} />
+            })}
+        </div>
+    );
+};
+
+export default Board;
--- src/components/board/board.test.js
+++ src/components/board/board.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, {shallow} from 'enzyme';
 import Board from './board';
+import Square from '../square/square';
 
 describe('Board component', () => {
   Enzyme.configure({ adapter: new Adapter() });
@@ -12,22 +13,40 @@ describe('Board component', () => {
 
   it('should not render any image on empty state', () => {
     const board = shallow(<Board state={emptyState} />);
+    const squares = board.find(Square);
 
-    expect(board.find('.sprite-o-state').length).toEqual(0);
-    expect(board.find('.sprite-x-state').length).toEqual(0);
+    expect(squares.length).toEqual(9);
+    squares.forEach(square => {
+      expect(square.props().state === '-').toBe(true);
+    });
   });
 
   it('should render correct images for running state', () => {
-    const board = shallow(<Board state={runningState} />);
-
-    expect(board.find('.sprite-o-state').length).toEqual(2);
-    expect(board.find('.sprite-x-state').length).toEqual(1);
+    const board = shallow(<Board state={runningState} />),
+      squares = board.find(Square);
+    
+    expect(squares.length).toEqual(9);
+    
+    squares.forEach((square, index) => {
+      switch(index) {
+        case 0:
+        case 4:
+          expect(square.props().state === 'O').toBe(true);
+          break;
+        case 3:
+          expect(square.props().state === 'X').toBe(true);
+      }
+    });
   });
 
   it('should defaults to empty state for an invalid state', () => {
-    const board = shallow(<Board state={'OX'} />);
+    const board = shallow(<Board state='OX' />);
+    const squares = board.find(Square);
 
-    expect(board.find('.sprite-o-state').length).toEqual(0);
-    expect(board.find('.sprite-x-state').length).toEqual(0);
+    expect(squares.length).toEqual(9);
+
+    squares.forEach(square => {
+      expect(square.props().state === '-').toBe(true);
+    });
   });
 });
