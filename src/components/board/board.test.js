25f652b4e1f0c1d5fa69d881acbd2db18045b3bd
Arlindo Neto <arlindosilvaneto@gmail.com>
2019-03-12 11:54:05 -0300

    TTT-0002 [Arlindo] :: Board component tests

--- src/components/board/board.test.js
+++ src/components/board/board.test.js
@@ -2,8 +2,32 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, {shallow} from 'enzyme';
+import Board from './board';
 
 describe('Board component', () => {
   Enzyme.configure({ adapter: new Adapter() });
 
+  const emptyState = '---------',
+    runningState = 'O--XO----';
+
+  it('should not render any image on empty state', () => {
+    const board = shallow(<Board state={emptyState} />);
+
+    expect(board.find('.sprite-o-state').length).toEqual(0);
+    expect(board.find('.sprite-x-state').length).toEqual(0);
+  });
+
+  it('should render correct images for running state', () => {
+    const board = shallow(<Board state={runningState} />);
+
+    expect(board.find('.sprite-o-state').length).toEqual(2);
+    expect(board.find('.sprite-x-state').length).toEqual(1);
+  });
+
+  it('should defaults to empty state for an invalid state', () => {
+    const board = shallow(<Board state={'OX'} />);
+
+    expect(board.find('.sprite-o-state').length).toEqual(0);
+    expect(board.find('.sprite-x-state').length).toEqual(0);
+  });
 });
